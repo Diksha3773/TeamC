fe4ea422da4daafeb34a5176870b33c5a3cd938d
Ayush Sharma <87691871+Ayush-13-02@users.noreply.github.com>
2022-06-14 17:07:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 // import { Container } from 'postcss';
 import './App.css';
 import Director from './components/director';
+import Test from "./component/Placement";
+import Checker from "./component/Institute";
 
 function App() {
   return(
     
 <Director></Director>
-
+<Test/>
+<Checker/>
   
   );
 }
