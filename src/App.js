c153ca9e10406dc7c1a337c06c9484d4364035b5
Ayush Sharma <87691871+Ayush-13-02@users.noreply.github.com>
2022-06-17 22:11:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import Director from './components/director';
 import Count from './components/Placement';
-import Checker from './components/Institute';
 import Navbar from './components/Navbar';
-
-
+import Checker from './components/Institute';
 
 function App() {
   return (
