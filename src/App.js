975504bf16af087becbd113d7f73e8a13fd71d35
Ayush Sharma <87691871+Ayush-13-02@users.noreply.github.com>
2022-06-17 22:08:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Director from './components/director';
 import Count from './components/Placement';
 import Checker from './components/Institute';
+import Navbar from './components/Navbar';
 
 
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Director/>
       <Count/>
       <Checker/>
