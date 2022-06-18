2ef689596a9a01be4ccdfcd8bd98f0ba648463ef
Diksha <diksha3773@gmail.com>
2022-06-18 11:06:43 +0530

    department section added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import Director from './components/director';
 import Count from './components/Placement';
 import Checker from './components/Institute';
+import Department from './components/department';
 
 
 
 function App() {
   return (
     <div>
-      <Director/>
+       <Director/>
       <Count/>
-      <Checker/>
+      <Checker/> 
+      
     </div>
+//     <div>
+//  <Department/>
+//     </div>
   );
 }
 
---
+++ src/components/department.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Department = () => {
+  return (
+    <div >
+
+<img className=' ' src="https://www.nitj.ac.in/ece/images/faculty/mamta_mam.jpeg"  title="HOD" alt="HOD"></img>
+
+
+    </div>
+  )
+}
+
+export default Department
