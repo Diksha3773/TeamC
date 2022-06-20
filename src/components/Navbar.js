a7b27531b3b0ccb5e11f8738c3bed09b93ef8f7d
Ayush Sharma <87691871+Ayush-13-02@users.noreply.github.com>
2022-06-20 09:40:03 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,30 +3,33 @@ import logo from './Img/logo.png'
 import comp from './Vedio/comp.mp4'
 import Hambur from './Img/menu.png'
 import Imagesroll from './Imagesroll'
+import Tippy from '@tippyjs/react'
+import 'tippy.js/dist/tippy.css'
+import Clock from './Clock'
 function Navbar() {
     const [open, setOpen] = useState(false);
     const [openabout, setOpenabout] = useState(false)
     const [openreasearch, setOpenreasearch] = useState(false)
     const [openpupil, setOpenpupil] = useState(false);
-    const [openacad, setOpenacad] = useState(false)
+    const [openacad, setOpenacad] = useState(false);
+    const [opencheck, setOpencheck] = useState(false)
     const [width, setWidth] = useState(getWindowSize())
     const myref = useRef();
     const getListsize = () => {
-        // const width1 = myref.current.clientWidth;
         setWidth(getWindowSize);
     }
 
     useEffect(() => {
         window.addEventListener('resize', getListsize);
-        return()=>{
-            window.removeEventListener('resize',getListsize);
+        return () => {
+            window.removeEventListener('resize', getListsize);
         };
     }, []);
-    console.log(width,'hi')
+    console.log(opencheck, 'nav');
     return (
         <>
             <div className='flex flex-col h-auto w-full lg:h-screen' ref={myref}>
-                <div className='h-24 p-2 py-4 flex bg-zinc-900 text-white w-full items-center bg-gradient-to-tr'>
+                <div className='h-24 p-2 py-4 flex bg-zinc-900 text-white w-full items-center justify-between'>
                     <div className='flex items-center my-2'>
                         <img src={logo} className='h-20' alt='' />
                         <div className='mx-2'>
@@ -37,10 +40,24 @@ function Navbar() {
                             <div>National Institute of Technology Jalandhar</div>
                         </div>
                     </div>
+                    <Tippy content={<span className='text-white'>Background image scroller(automatic)</span>} placement='bottom'>
+                        <div className='md:flex hidden justify-center items-center p-1 w-6 h-6 bg-blue-600 rounded-full display'>
+                            <input type='checkbox' id='time' onClick={() => { setOpencheck(!opencheck) }} className='h-6 w-6 rounded-full' />
+                        </div>
+                    </Tippy>
+                    <div className='hidden md:block'><Clock /></div>
                 </div>
-                <button className='p-3 bg-zinc-900 flex items-center md:hidden' onClick={() => { setOpen(!open) }} >
-                    <img src={Hambur} alt="..." className='h-8 w-8 m-1 mx-2 p-1 rounded-lg hover:border' />
-                </button>
+                <div className='flex justify-between items-center border bg-zinc-900'>
+                    <button className='p-3  flex items-center md:hidden' onClick={() => { setOpen(!open) }} >
+                        <img src={Hambur} alt="..." className='h-8 w-8 m-1 mx-2 p-1 rounded-lg hover:border' />
+                    </button>
+                    <Tippy content={<span className='text-white'>Background image scroller(automatic)</span>} placement='bottom'>
+                        <div className='flex md:hidden justify-center items-center p-1 w-6 h-6 bg-blue-600 rounded-full display'>
+                            <input type='checkbox' id='time' onClick={() => { setOpencheck(!opencheck) }} className='h-6 w-6 rounded-full' />
+                        </div>
+                    </Tippy>
+                    <div className='block md:hidden m-2'><Clock /></div>
+                </div>
                 <div className='flex-col md:flex w-full justify-center md:items-center md:flex-row' style={open ? { dispay: "flex" } : width < 768 ? { display: "none" } : { display: "flex" }}>
                     <div className='group bg-neutral-800 text-white shadow-sm p-2 m-0 md:border-none md:bg-white md:text-black'><a href='/' className='my-2 mx-4 font-medium hover:text-blue-600'>Home</a></div>
                     <div className='group bg-neutral-800 text-white shadow-sm p-2 m-0 md:border-none md:bg-white md:text-black'>
@@ -54,7 +71,7 @@ function Navbar() {
                         </div>
                     </div>
                     <div className='group bg-neutral-800 text-white shadow-sm p-2 m-0 md:border-none md:bg-white md:text-black'>
-                        <span className='my-2 mx-4 font-medium hover:text-blue-600 cursor-pointer'onClick={()=>{setOpenacad(!openacad)}}>Academics</span>
+                        <span className='my-2 mx-4 font-medium hover:text-blue-600 cursor-pointer' onClick={() => { setOpenacad(!openacad) }}>Academics</span>
                         <div className='absolute right-[15%] md:right-auto z-10 mx-2 hidden border rounded-b-md bg-neutral-900 text-white md:group-hover:block' style={width < 768 ? openacad ? { display: "block" } : { dispay: "none" } : null}>
                             <div ><a href='/' className='flex w-full p-2 font-medium hover:bg-neutral-600'>Programmes</a></div>
                             <div ><a href='/' className='flex w-full p-2 font-medium hover:bg-neutral-600'>Academic Coordinates</a></div>
@@ -64,8 +81,8 @@ function Navbar() {
                         </div>
                     </div>
                     <div className='group bg-neutral-800 text-white shadow-sm p-2 m-0 md:border-none md:bg-white md:text-black'>
-                        <span className='my-2 mx-4 font-medium hover:text-blue-600 cursor-pointer' onClick={()=>setOpenpupil(!openpupil)}>People</span>
-                        <div className='absolute right-1/2 md:right-auto z-10 mx-2 hidden border rounded-b-md bg-neutral-900 text-white md:group-hover:block'style={width < 768 ? openpupil ? { display: "block" } : { dispay: "none" } : null}>
+                        <span className='my-2 mx-4 font-medium hover:text-blue-600 cursor-pointer' onClick={() => setOpenpupil(!openpupil)}>People</span>
+                        <div className='absolute right-1/2 md:right-auto z-10 mx-2 hidden border rounded-b-md bg-neutral-900 text-white md:group-hover:block' style={width < 768 ? openpupil ? { display: "block" } : { dispay: "none" } : null}>
                             <div><a href='/' className='flex justify-center w-full p-2 font-medium hover:bg-neutral-600'>Faculty</a></div>
                             <div><a href='/' className='flex justify-center p-2 w-full font-medium hover:bg-neutral-600'>PhD Scholars</a></div>
                             <div><a href='/' className='flex justify-center p-2 w-fullfont-medium hover:bg-neutral-600'>Students</a></div>
@@ -74,8 +91,8 @@ function Navbar() {
                         </div>
                     </div>
                     <div className='group bg-neutral-800 text-white shadow-sm p-2 m-0 md:border-none md:bg-white md:text-black'>
-                        <span className='my-2 mx-4 font-medium hover:text-blue-600 cursor-pointer'onClick={()=>setOpenreasearch(!openreasearch)}>Research and Labs</span>
-                        <div className='absolute z-10 mx-2 hidden border rounded-b-md bg-neutral-500 text-white md:group-hover:grid grid-cols-3  md:group-hover:grid-cols-2 lg:group-hover:grid-cols-3'style={width < 768 ? openreasearch ? { display: "grid" } : { dispay: "none" } : null}>
+                        <span className='my-2 mx-4 font-medium hover:text-blue-600 cursor-pointer' onClick={() => setOpenreasearch(!openreasearch)}>Research and Labs</span>
+                        <div className='absolute z-10 mx-2 hidden border rounded-b-md bg-neutral-500 text-white md:group-hover:grid grid-cols-3  md:group-hover:grid-cols-2 lg:group-hover:grid-cols-3' style={width < 768 ? openreasearch ? { display: "grid" } : { dispay: "none" } : null}>
                             <div className='block w-auto flex-initial m-2 rounded hover:text-blue-700'>
                                 <div className='w-28 h-28 relative flex flex-col min-w-0 hover:border rounded'>
                                     <div className='flex-auto text-center '>
@@ -140,7 +157,7 @@ function Navbar() {
                     </div>
 
                 </div>
-                <Imagesroll />
+                <Imagesroll opencheck={opencheck} />
             </div>
         </>
     )
