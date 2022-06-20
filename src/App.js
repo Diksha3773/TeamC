d9afb6d8c7b1fb88a9acbad589fba7b0934db5a7
Diksha <diksha3773@gmail.com>
2022-06-20 22:22:26 +0530

    department website updated with body of website

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Director from './components/director';
 import Count from './components/Placement';
 import Navbar from './components/Navbar';
 import Checker from './components/Institute';
-import {Departmentupper,Departmentlower} from './components/department';
+import {Departmentupper, Departmentlower} from './components/department';
+
 
 function App() {
   return (
@@ -18,14 +19,19 @@ function App() {
 
 // components on department website
     <div>
-  <Navbar/>  
-  <Departmentupper/>
-    </div>
-  // <div>
+  <Navbar/>   
+     <Departmentupper name="CSE" nameofhod=" ~Dr Geeta Sikka"
+     introduction="Located in Jalandhar district of Punjab, NIT Jalandhar enjoys a really scenic environment 
+    and pleasant weather. Established in the year 1987, as REC Jalandhar, NIT Jalandhar has been 
+    declared as the Institute of National Importance under the Act of Parliament, 2007. Established
+     in 1990 as the Department of Computer Science & Engineering, we have an excellent & rich history
+      and an outstanding record of contributions to the profession and community." hod='https://www.nitj.ac.in/images/faculty/20071962443.jpg'
+      departmentimage='https://www.nitj.ac.in/cse/images/carousel/001_1.jpeg'/>
+      <Departmentlower/>
+  </div>
   
-  //  <Departmentlower/>
-    
-  // </div>
+
+  
 
 
 
--- src/components/department.js
+++ src/components/department.js
@@ -1,67 +1,67 @@
 import React from 'react'
-import uuid from 'react-uuid'
+import uuid from 'react-uuid';
 
-const Departmentupper = () => {
-  return (
-    <div>
-
-
-      {/* HOD SECTION */}
-<div className='w-fit border-2 border-blue-400 mt-5 ml-[560px]  relative left-[430px] '>
-  <h1 className='text-2xl ml-10'>Head Of Department</h1>  
-<img className='h-[380px] w-[300px] ' src='https://www.nitj.ac.in/images/faculty/20071962443.jpg' width={400} ></img>
-<p className='float-right'> ~Dr Geeta Sikka</p>
-</div>
-
-{/* Department Picture */}
-<div className='w-fit border-2 border-blue-400 relative bottom-[400px] ml-10 '>
-<img className='h-[400px] w-[432px] ' src='https://www.nitj.ac.in/cse/images/carousel/001_1.jpeg'  ></img>
-</div >
-
-{/* Department Introduction */}
-<div className=' border-2 border-blue-400 relative bottom-[390px] h-[405px] ml-10 w-[495px] relative left-[435px] bottom-[805px] bg-[#045F5F] text-white'>
-  <div className='text-l p-5 text-justify '>
-    <h1 className='text-4xl pb-5'> Department of CSE</h1>
-    Located in Jalandhar district of Punjab, NIT Jalandhar enjoys a really scenic environment 
-    and pleasant weather. Established in the year 1987, as REC Jalandhar, NIT Jalandhar has been 
-    declared as the Institute of National Importance under the Act of Parliament, 2007. Established
-     in 1990 as the Department of Computer Science & Engineering, we have an excellent & rich history
-      and an outstanding record of contributions to the profession and community.</div>
-    <button className='bg-white text-green-500 ml-44 p-4 rounded-2xl hover:bg-orange-500 text-white '>Read More  &rarr;</button>
-</div>
-
-     </div>
-  )
-}
-
-
-
-// Add news in this array between quotes
 const news =['Congratulations to Yogita, B. Tech Final year, ECE Department for getting placed at Atlassian at a whopping CTC of 52LPA through an off-campus drive.',
-                'Online Short Term Course on “Research Trends in Communication and Signal Processing” (December 20-24, 2021)',
-              ' Dr Sukwinder Singh registered a start-up “RFvis Technologies”under TBI at NITJ (March, 2022)',
-            'Research Article Tulika Chawla, Mamta Khosla and Balwinder Raj, “Extended Gate to source overlap Heterojunction Vertical TFET: Design, analysis and optimization with process parameter variations” got published in “Materials Science in Semiconductor Processing”, vol.145, 2022. ( SCI, Impact Factor 3.927)',
-              'Congratulations to Prof. Binod Kumar Kanaujia for getting research funding for R&amp;D project “Design and Development of Biologically Inspired Retinal Prosthesis Based Biomedical Implant to Restore Vision to the Blind” worth Rs. 23.28 Lacs under the Empowerment and Equity Opportunities for Excellence in Science Grant Scheme of SERB, Government of India (March, 2022)' ];
+ 'Online Short Term Course on “Research Trends in Communication and Signal Processing” (December 20-24, 2021)',
+' Dr Sukwinder Singh registered a start-up “RFvis Technologies”under TBI at NITJ (March, 2022)',
+'Research Article Tulika Chawla, Mamta Khosla and Balwinder Raj, “Extended Gate to source overlap Heterojunction Vertical TFET: Design, analysis and optimization with process parameter variations” got published in “Materials Science in Semiconductor Processing”, vol.145, 2022. ( SCI, Impact Factor 3.927)',
+'Congratulations to Prof. Binod Kumar Kanaujia for getting research funding for R&amp;D project “Design and Development of Biologically Inspired Retinal Prosthesis Based Biomedical Implant to Restore Vision to the Blind” worth Rs. 23.28 Lacs under the Empowerment and Equity Opportunities for Excellence in Science Grant Scheme of SERB, Government of India (March, 2022)',
+'Kirandeep Kaur Sahota (Batch 2017 passed out) brought glory and pride to the institute and department of ECE by securing AIR-331 in UPSC Civil Service Examination, 2020' ];
 
+ const Departmentupper = (props) => {
+  return (
+    <div className='flex flex-col 
+                    sm:flex-row justify-evenly '>
 
+<div className='    m-2 border-2  overflow-scroll overflow-x-hidden
+                   md:h-[500px]
+                   lg:h-auto'> 
+<img  src={props.departmentimage} ></img>
+<div >
+<h1 className='underline text-center '> <b>Department of {props.name}</b></h1>
+  <p className='p-3 text-justify '> {props.introduction} </p>
+    <button className='float-right text-green-500 '>Read More  &rarr;</button>
+    </div>
+</div>
 
- const Departmentlower=()=>{
- return(
-  <div className=' border-2 border-blue-400 relative bottom-[780px] ml-10 w-[600px] h-96 overflow-scroll p-5 '> 
-   <i className='text-6xl font-serif pb-5'>What's New ?</i>
+<div className='hidden sm:block m-10 
+               md: m-1  w-[2000px]'>
+<h1 className='float-left text-3xl text-[#045F5F] '><b>Head Of Department</b></h1>  
+<img className=' border-8 border-teal-900 rounded-md' src={props.hod} width={400} ></img>
+<p className='float-left text-teal-900'>{props.nameofhod}</p>
+</div>
+
+  </div>
+  
+  )
+ }
+
+ 
+ const Departmentlower= (props) => {
+  return (
+<div className=' h-[500px]  mt-10  m-3 flex flex-col 
+                    md:flex-row justify-evenly
+                   '>
+<div className=' overflow-scroll overflow-x-hidden lg:w-96 xl:w-[600px]'>
+<i className='text-2xl font-serif '><b>What's New ?</b></i>
    <ol>
   {
- 
-  news.map((n)=> <li key={uuid()} className='list-disc border-b-2 border-gray-500'>{n}</li>)
+  news.map((n)=> <li key={uuid()} className='list-disc border-b-2  border-gray-300 text-sm p-3 list-inside '>{n}</li>)
   }
   </ol>
+<button className='bg-[#21b684] text-white float-right rounded-lg p-1'>View All</button>
+</div>
+
+
+<iframe className=' mt-20 w-44 rounded-2xl sm: ml-20  h-96 md:w-[560px] lg: h-[315px] w-[460px]
+
+' src="https://www.youtube.com/embed/fd2hUOmm8kA?controls=0&amp;start=280&mute=1" title="NITJ" ></iframe>
+
+</div>
   
-  </div>
- )
-}
 
-
-export 
-{
+    )
+   }
+   export{
   Departmentupper,Departmentlower
-};
+}
