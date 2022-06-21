3ecfc178fef0511b10aeff339ab0002b303f088a
Ayush Sharma <87691871+Ayush-13-02@users.noreply.github.com>
2022-06-21 14:54:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Director from './components/director';
 import Count from './components/Placement';
 import Navbar from './components/Navbar';
 import Checker from './components/Institute';
-import {Departmentupper, Departmentlower} from './components/department';
+import { Departmentupper, Departmentlower } from './components/Department';
 
 
 function App() {
@@ -17,19 +17,19 @@ function App() {
 
 
 
-// components on department website
+    // components on department website
     <div>
-  <Navbar/>   
-     <Departmentupper name="CSE" nameofhod=" ~Dr Geeta Sikka"
-     introduction="Located in Jalandhar district of Punjab, NIT Jalandhar enjoys a really scenic environment 
+      <Navbar />
+      <Departmentupper nameofhod=" ~Dr Geeta Sikka" hod='https://www.nitj.ac.in/images/faculty/20071962443.jpg'
+      />
+      <Departmentlower name="CSE" introduction="Located in Jalandhar district of Punjab, NIT Jalandhar enjoys a really scenic environment 
     and pleasant weather. Established in the year 1987, as REC Jalandhar, NIT Jalandhar has been 
     declared as the Institute of National Importance under the Act of Parliament, 2007. Established
      in 1990 as the Department of Computer Science & Engineering, we have an excellent & rich history
-      and an outstanding record of contributions to the profession and community." hod='https://www.nitj.ac.in/images/faculty/20071962443.jpg'
-      departmentimage='https://www.nitj.ac.in/cse/images/carousel/001_1.jpeg'/>
-      <Departmentlower/>
-  </div>
-  
+      and an outstanding record of contributions to the profession and community."
+        departmentimage='https://www.nitj.ac.in/cse/images/carousel/001_1.jpeg' />
+    </div>
+
   );
 }
 
