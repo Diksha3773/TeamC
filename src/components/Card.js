6582717c4c607484c86808f6565a599317a9140c
Ayush Sharma <87691871+Ayush-13-02@users.noreply.github.com>
2022-06-22 10:16:29 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,6 @@
 import React from 'react'
 const Card = ({ title, image, body }) => {
     return (
-        // <div className=" max-h-fit lg:w-[90%] bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 aspect-square">
-        //     <div className='flex justify-center items-center'>
-        //         <a href="#">
-        //             <img className="rounded-t-lg h-60" src={image} alt="" />
-        //         </a>
-        //     </div>
-        //     <div className="p-5">
-        //         <a href="#">
-        //             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-        //         </a>
-        //         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{body}</p>
-        //         <div className='block m-2 p-2'>
-        //             <button className=' float-right text-green-500 hover:font-semibold'>Read More  &rarr;</button>
-        //         </div>
-        //     </div>
-        // </div>
         <div className="container w-full mx-auto flex flex-col">
             <div v-for="card in cards" className="flex flex-col lg:flex-row overflow-hidden bg-white rounded-lg shadow-xl  mt-4 w-100 mx-2">
                 <div className="h-64 w-auto">
