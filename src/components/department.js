38d15e01007105f98eb2522172d614068c901cf9
Ayush-13-02 <ayushsharma13jun@gmail.com>
2022-06-25 19:29:05 +0530

    NitJ

--- src/components/department.js
+++ src/components/department.js
@@ -100,7 +100,7 @@ const Departmentupper = (props) => {
 const Departmentlower = (props) => {
     return (
         <div className='m-3 mt-10 flex flex-col md:flex-row justify-center items-center md:justify-evenly overflow-y-hidden'>
-            <div className="bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 md:w-1/2 ">
+            <div className="bg-white rounded-lg border border-gray-200 shadow-md md:w-1/2 ">
                 <div className='flex justify-center items-center'>
                     <a href="/">
                         <img className="rounded-t-lg" src={props.departmentimage} alt="" />
@@ -111,7 +111,7 @@ const Departmentlower = (props) => {
                     <a href="/">
                         <h1 className='underline text-center text-2xl m-2'> <b>Department of {props.name}</b></h1>
                     </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.introduction}</p>
+                    <p className="mb-3 font-normal text-gray-700 ">{props.introduction}</p>
                     <div className='block m-2 p-2'>
                         <button className=' float-right text-green-700 hover:font-semibold hover:text-blue-700 '>Read More  &rarr;</button>
                     </div>
