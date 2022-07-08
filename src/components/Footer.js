79e122c3114f48ecdc9d8ff60c3deecb83fda2f2
Ayush-13-02 <ayushsharma13jun@gmail.com>
2022-07-09 00:08:51 +0530

    Nitj

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ function Footer({ scroll,handlescroll }) {
     return (
 
         <>
-            <footer className='bg-gray-200' ref={footref}>
+            <footer className='bg-gray-200'>
                 <div className="p-10 bg-neutral-700 text-gray-200">
                     <div className="max-w-7xl mx-auto">
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
@@ -33,7 +33,7 @@ function Footer({ scroll,handlescroll }) {
                                     <li className="pb-1"><a href="/" className="text-xl hover:text-yellow-500">Academic Calendar</a></li>
                                 </ul>
                             </div>
-                            <div >
+                            <div ref={footref}>
                                 <h4 className="pt-32 text-2xl text-center underline">Contact Us</h4>
                                 <ul className="mt-2 text-center">
                                     <li className="pb-1 text-xl">Phone : +123456789</li>
