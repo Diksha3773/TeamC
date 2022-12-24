24c265755c5dbe8c8327d41794e9ce4dfe456043
parteekcoder <100110395+parteekcoder@users.noreply.github.com>
2022-12-24 13:44:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,9 @@ const _dirname = path.resolve();
 app.use('/:dept',checkDepartment);
 app.use('/:dept', routesHandler);
 
-app.use(express.static(path.join('/opt/render/project/src/', '/frontend/build')));
+app.use(express.static(path.join('/opt/render/project/src', '/frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join('/opt/render/project/src/', '/frontend/build/index.html'))
+  res.sendFile(path.join('/opt/render/project/src', '/frontend/build/index.html'))
 );
 mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true})
 .then( () => {
